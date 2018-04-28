02745bc1ce40d3e863655af37204d61acbcdb0dc
tinn2015 <229813475@qq.com>
2018-04-28 15:19:24 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>{{name}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+      return {
+        name: 'Tinn'
+      }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
