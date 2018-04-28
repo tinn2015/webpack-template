d51f2699f8a691be9ef6f953e59ba75e53255ed0
tinn2015 <229813475@qq.com>
2018-04-28 18:32:05 +0800

    Update webpack.config.js

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,14 +1,48 @@
 const path = require('path')
+const {VueLoaderPlugin} = require('vue-loader')
 
 module.exports = {
   mode: 'production',
-  entry: '../src/main.js',
+  entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   module: {
-
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        loader: 'vue-loader',
+        // options: {
+        //   postcss: [
+        //     require('autoprefixer')({
+        //       browsers: ['last 6 versions']
+        //     })
+        //   ]
+        // }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [path.resolve('src')]
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            query: {
+              limit: 10000
+            }
+          }
+        ]
+      }
+    ]
   },
   resolve: {
     modules: ['node_modules'],
@@ -23,6 +57,6 @@ module.exports = {
   devtool: 'cheap-module-source-map',
   target: 'web',
   plugins: [
-
+    new VueLoaderPlugin()
   ]
 }
