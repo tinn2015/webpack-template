2e5ee2691c21559055badd51b19b7264557a12c7
tinn2015 <229813475@qq.com>
2018-05-23 00:04:06 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
     <h1>{{name}}</h1>
+    <ul style="height: 60px;overflow: auto">
+      <li style='height: 30px;'>dasdasdas</li>
+      <li style='height: 30px;'>dasdasdas</li>
+      <li style='height: 30px;'>dasdasdas</li>
+      <li style='height: 30px;' ref="lastLi" >dads<input type="text" id="lastli" style="opacity:0"></li>
+    </ul>
     <router-link to="/home">home</router-link>
     <router-link to="/">back</router-link>
     <router-view></router-view>
@@ -14,6 +20,9 @@ export default {
     return {
       name: 'Tinn'
     }
+  },
+  mounted () {
+    this.$el.querySelector('#lastli').focus()
   }
 }
 </script>
